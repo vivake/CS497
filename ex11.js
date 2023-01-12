b3fb7c3c462d704248178b709e3e264d244c4f5b
vivake <v94vake@gmail.com>
2023-01-11 20:28:58 -0500

    Add files via upload

---
+++ ex11.js
@@ -0,0 +1,34 @@
+let college = 'York College';
+const myName = 'Jane Doe';
+
+console.log(college);
+console.log(myName);
+
+const favoriteSubject = 'Math';
+//Console.log the text "My name is: " follows by myName variable.
+console.log("My name is: " + myName);
+//Console.log the text "My college is: " follows by college variable.
+console.log("My college is: "+ college);
+//Reassign the value stored in favoriteSubject to the string "Coding", is it possible?
+//favoriteSubject"Coding"; gives an error
+//can you console.log(number1) here?
+//console.log(number1); error as number` is not initialized.
+//Declare a new variable named 'number1' and give it the value of 10.
+let number1 = 10;
+//Declare a new variable named 'number2' and give it the value of 15.
+let number2 = 15;
+
+//console.log the summation of number1 and number2 in the following format
+//Expected output: The sum is 25
+console.log("The sum is "+(number1+number2));
+//console.log the product of number1 and number2 in the following format
+//Expected output: The product is 150
+console.log("The product is "+(number1*number2));
+
+//Declare a new variable named 'check' and give it the value of true (Boolean).
+let check=true;
+//console.log check and see the value, try also with false.
+console.log(check);
+
+check= false;
+console.log(check);
